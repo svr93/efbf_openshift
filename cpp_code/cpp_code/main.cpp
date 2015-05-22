89464e0499dd9c240631796742afa2eba762d77a
Владимир <svr93@i.ua>
2015-05-22 23:56:10 +0400

    replace_long_long

--- cpp_code/cpp_code/main.cpp
+++ cpp_code/cpp_code/main.cpp
@@ -11,18 +11,16 @@ using std::endl;
 using std::map;
 using std::vector;
 
-int main()
-{
-	typedef long long base;
-
-	base n;
+int main() {
+  base n = 0;
 	cin >> n;
 
 	map <base, unsigned> m;
-	factorize (n, m, (long long) 0);
+	factorize (n, m, (base) 0);
 
-	for (map <base, unsigned>::iterator i=m.begin(); i!=m.end(); ++i)
-		cout << i->first << ' ' << i->second << endl;
+	for (map <base, unsigned>::iterator i = m.begin(); i != m.end(); ++i) {
+    cout << i->first << ' ' << i->second << endl;
+  }
 
   return 0;
 }
