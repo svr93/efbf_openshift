20dd4dad031841f04ac89431ec10b7fbe0b5dddb
Владимир <svr93@i.ua>
2015-05-22 23:47:53 +0400

    factorization.h___ver1.0

--- cpp_code/cpp_code/factorization.h
+++ cpp_code/cpp_code/factorization.h
@@ -1,13 +1,23 @@
 #ifndef _FACTORIZATION_H_
 #define _FACTORIZATION_H_
 
-#include <algorithm>
-#include <cmath>
-#include <iostream>
+#include <algorithm> // for std::upper_bound
 #include <map>
 #include <vector>
 
-template <class T, class T2>
-void factorize (const T & n, std::map<T,unsigned> & result, T2 unused);
+using std::map;
+using std::vector;
+
+typedef long long base;
+
+/* ----- factorize ----- */
+
+template <typename T, typename T2>
+void factorize (const T& n, map<T, unsigned>& result, T2 unused);
+
+template
+void factorize <base, base> (const base& n, map<base, unsigned>& result, base unused);
+
+/* ----- /factorize ----- */
 
 #endif
