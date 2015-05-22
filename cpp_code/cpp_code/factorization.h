80bb276ec78168bfafe23ec341aecaf7599de5a7
Владимир <svr93@i.ua>
2015-05-22 16:31:57 +0400

    factorization.h___ver1.0

---
+++ cpp_code/cpp_code/factorization.h
@@ -0,0 +1,13 @@
+#ifndef _FACTORIZATION_H_
+#define _FACTORIZATION_H_
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <vector>
+
+template <class T, class T2>
+void factorize (const T & n, std::map<T,unsigned> & result, T2 unused);
+
+#endif
