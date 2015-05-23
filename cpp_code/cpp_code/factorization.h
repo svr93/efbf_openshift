450e4eccdd972642d6f98572cf7d2f6ce07e417e
Владимир <svr93@i.ua>
2015-05-23 14:35:12 +0400

    change_returning_value_type

--- cpp_code/cpp_code/factorization.h
+++ cpp_code/cpp_code/factorization.h
@@ -1,10 +1,12 @@
 #ifndef _FACTORIZATION_H_
 #define _FACTORIZATION_H_
 
+#include <iostream>
 #include <algorithm> // for std::upper_bound
 #include <map>
 #include <vector>
 
+using std::cout;
 using std::map;
 using std::vector;
 
@@ -13,10 +15,12 @@ typedef long long base;
 /* ----- factorize ----- */
 
 template <typename T, typename T2>
-void factorize (const T& n, map<T, unsigned>& result, T2 unused);
+vector<vector<base> >
+factorize (const T& n, map<T, unsigned>& result, T2 unused);
 
 template
-void factorize <base, base> (const base& n, map<base, unsigned>& result, base unused);
+vector<vector<base> >
+factorize <base, base> (const base& n, map<base, unsigned>& result, base unused);
 
 /* ----- /factorize ----- */
 
