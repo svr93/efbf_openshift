9803b4428dd77ae87dd31ab76b60aa4e87f19493
Владимир <svr93@i.ua>
2015-05-23 15:06:02 +0400

    add_methods_info_output

--- cpp_code/cpp_code/main.cpp
+++ cpp_code/cpp_code/main.cpp
@@ -13,12 +13,33 @@ using std::vector;
 
 int main() {
   base n = 0;
-	cin >> n;
+  cin >> n;
 
-	map <base, unsigned> m;
-	factorize (n, m, (base) 0);
+  map <base, unsigned> m;
+  vector<vector<base> >methods; // need replace base-->unsigned
 
-	for (map <base, unsigned>::iterator i = m.begin(); i != m.end(); ++i) {
+  methods = factorize (n, m, (base) 0);
+
+  for (vector<vector<base> >::iterator i = methods.begin();
+       i != methods.end();
+       ++i) {
+
+    for (vector<base>::iterator j = (*i).begin();
+         j != (*i).end();
+         ++j) {
+
+      cout << (*j) << ';';
+    }
+
+    cout << endl;
+  }
+
+  cout << "___" << endl;
+
+  for (map <base, unsigned>::iterator i = m.begin();
+       i != m.end();
+       ++i) {
+
     cout << i->first << ' ' << i->second << endl;
   }
 
