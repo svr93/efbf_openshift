5a15369a9a0a85ec6ec38786c8a23995cac692aa
Владимир <svr93@i.ua>
2015-05-23 17:56:58 +0400

    include_new_start_commands

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 iptables -I INPUT -p tcp --dport 80 -m state --state NEW -j ACCEPT
+
+cp -f cpp_code/cpp_code/factorization.h \
+      applications/main/app/api/info.json/factorization.h
+
+cp -f cpp_code/cpp_code/factorization.cpp \
+      applications/main/app/api/info.json/factorization.cc
+
 mainPath=$(pwd)
 cd applications/main/app/api/info.json
 . build.sh
